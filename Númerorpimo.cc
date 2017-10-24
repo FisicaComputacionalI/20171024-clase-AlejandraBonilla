838993cb3f32ca2a7c5b5e4e6ee50d7cfd4d74fd
AlejandraBonilla <31254521+AlejandraBonilla@users.noreply.github.com>
2017-10-24 16:27:15 -0500

    Create Númerorpimo.cc
    Número primo

---
+++ Númerorpimo.cc
@@ -0,0 +1,27 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  long N=12;
+  long Flag=0;
+  long divisor=0;
+  cout<<"inserte un numero"<<endl;
+  cin>>N;
+  for(long i=2; i<=N/2; i++)
+    { 
+      if(N%i==0){
+	Flag=1;
+        divisor=i;
+        break;
+      }
+    }
+  if(Flag==1){
+    cout<<"tu numero no es primo"<<endl;
+  cout<<"porque se divide entre"<<divisor<<endl;
+  }
+ else
+    cout<<"tu numero es primo"<<endl;
+  return 0;
+  }
+  
